e88327c72cb44e805c5bb88db57ee5220e7cb452
divkumarsingh <divyanshusingh1101@gmail.com>
2026-07-25 14:04:38 +0000

    set up trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { caller } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
+export default async function Home() {
 
-export default function Home() {
+  const queryClient = getQueryClient();
 
-  
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button variant={"destructive"}>Click me</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading</p>}>
+          <Client/>
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
